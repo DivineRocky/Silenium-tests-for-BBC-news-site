881585033a5ad49f81fbbde808a38105f161fa25
Meow <Meow@MEOW>
2019-11-03 19:47:46 +0200

    Add project files.

---
+++ Test Automation EPAM/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test Automation EPAM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test Automation EPAM")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("308d1a84-4970-41fc-9042-de8409393116")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test Automation EPAM/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Automation_EPAM
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    public class UnitTest2
+    {
+        [IWebDriver]
+        public void IWebDriver()
+            IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.bbc.com");
+    }
+
+
+}
+
